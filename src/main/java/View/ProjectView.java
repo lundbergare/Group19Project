4f7cea313f258d12729a502d5f86e6b0d26db027
Narges-nazeri <narges.naz07@gmail.com>
2023-11-10 10:17:32 +0100

    new changes
    Pls work

--- src/main/java/View/ProjectView.java
+++ src/main/java/View/ProjectView.java
@@ -14,7 +14,7 @@ public class ProjectView {
     private JPanel currentScreen; // The current screen
 
     public ProjectView() {
-        frame = new JFrame("Super Mario Game");
+        frame = new JFrame("Super Smurf Game");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
         frame.setLocationRelativeTo(null);
