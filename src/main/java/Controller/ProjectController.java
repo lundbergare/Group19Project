ee6d77cbc133c95799c291f4164a1ea857369df8
Kasper Lundberg <kasperlundberg1@gmail.com>
2023-11-13 21:18:02 +0100

    MVC change
    Think it is more like to MVC (?)

--- src/main/java/Controller/ProjectController.java
+++ src/main/java/Controller/ProjectController.java
@@ -1,14 +1,50 @@
 package Controller;
 
-import Model.ProjectModel;
+import View.LevelSelectorView;
 import View.ProjectView;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class ProjectController {
-    private ProjectModel model;
     private ProjectView view;
 
-    public ProjectController(ProjectModel model, ProjectView view) {
-        this.model = model;
+    public ProjectController(ProjectView view) {
         this.view = view;
+
+        // Set up action listeners in the controller
+        view.addLevelButtonListener(new LevelButtonListener());
+        view.addQuitButtonListener(new QuitButtonListener());
+    }
+
+    // ActionListener for "Start Game" button
+    class LevelButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            showNewScreen();
+        }
+    }
+
+    // ActionListener for "Quit" button
+    class QuitButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.exit(0);
+        }
+    }
+
+    private void showNewScreen() {
+        view.showNewScreen(new LevelSelectorView(new BackButtonListener()));
+    }
+
+    class BackButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            showPreviousScreen();
+        }
+    }
+
+    private void showPreviousScreen() {
+        view.showPreviousScreen();
     }
 }
--- src/main/java/View/LevelSelectorView.java
+++ src/main/java/View/LevelSelectorView.java
@@ -2,21 +2,17 @@ package View;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class LevelSelectorView extends JPanel {
     private JButton backButton;
 
     public LevelSelectorView(ActionListener backButtonListener) {
-        // Add components and content for the new screen
-        JLabel label = new JLabel("This is the newer screen");
-        add(label);
 
-        // Add a "Back" button to return to the previous screen
+        setLayout(new FlowLayout(FlowLayout.LEFT, 20, 650));
+
         backButton = new JButton("Back");
         backButton.addActionListener(backButtonListener);
         add(backButton);
-        System.out.print("New screen now");
     }
 }
